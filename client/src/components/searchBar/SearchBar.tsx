79a91b3f2345af4bcae3d9324c256c58a2631f57
tomaszsliwakowski <tomek08082002@gmail.com>
2024-05-04 13:49:16 +0200

    home page update

--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
+type QueryType = {
+  type: string;
+  location: string;
+  minPrice: number;
+  maxPrice: number;
+};
+
+const types = ["buy", "rent"];
+
 export default function SearchBar() {
+  const [query, setQuery] = useState<QueryType>({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (newType: string) => {
+    setQuery((prev) => ({ ...prev, type: newType }));
+  };
+
   return (
     <div className="searchBar">
       <div className="type">
-        <button>Buy</button>
-        <button>Rent</button>
+        {types.map((type, id) => (
+          <button
+            className={query.type === type ? "active" : ""}
+            key={id}
+            onClick={() => switchType(type)}
+          >
+            {type}
+          </button>
+        ))}
       </div>
       <form>
         <input type="text" name="location" placeholder="City Location" />
